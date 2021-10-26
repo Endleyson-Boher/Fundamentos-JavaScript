b95f2e20db5b128e50022729615aa55b896b4a7c
Endleyson Boher <86278674+Endleyson-Boher@users.noreply.github.com>
2021-10-26 20:09:10 -0300

    Fundamentos JavaScript
    Fundamentos de JS

---
+++ fundamentos/operadores/desafio_logicos.js
@@ -0,0 +1,33 @@
+//====================================Minha resposta======================================
+
+// Duas expressões resultar em TRUE (Aritiméticos/Relacionais/Lógicos)
+
+let verdadeira1 = 2 - 2;
+let verdadeira2 = 4 / 2;
+
+console.log(verdadeira1 != 1 && verdadeira1 != 2);
+console.log(verdadeira2 == 2 || verdadeira2 != 2);
+
+// Duas expressões resultar em FALSE (Aritiméticos/Relacionais/Lógicos)
+let falso1 = 2 - 2;
+let falso2 = 4 / 2;
+
+console.log(falso1 != 1 && falso1 !=0);
+console.log(!falso2 == 2);
+
+//====================================Resposta do professor======================================
+console.log( );
+// Duas expressões resultar em TRUE (Aritiméticos/Relacionais/Lógicos)
+
+ verdadeira1 = 2 + 5 >= 7 && 8 >2;
+ verdadeira2 = !(6 * 6 < 3);
+
+console.log(verdadeira1);
+console.log(verdadeira2);
+
+// Duas expressões resultar em FALSE (Aritiméticos/Relacionais/Lógicos)
+ falso1 = 8 - 4 > 12 || false || 15 === '15';
+ falso2 = !(7 != 7 || '3' !== 3);
+
+console.log(falso1);
+console.log(falso2);
