5ae5bba9e55d780d958cf15bbfc484080620325e
Endleyson Boher <86278674+Endleyson-Boher@users.noreply.github.com>
2021-11-11 20:38:14 -0300

    Fundamentos JavaScript

--- fundamentos/controle/if_else_2.js
+++ fundamentos/controle/if_else_2.js
@@ -1,10 +1,10 @@
 const hora = 11;
 let saudacao;
 
-if(hora < 12) {
+if (hora < 12) {
     saudacao = 'Bom dia!';
 } else {
-    saudacao = 'Boa tarde';
+    saudacao = 'Boa tarde!';
 }
 
 console.log(saudacao);
